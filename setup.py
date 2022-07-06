bac1a4ab2998813feae4b75bf2a4cb3b9216e145
prakshsing <lalganjpraksh@gmail.com>
2022-07-06 22:33:30 +0530

    code commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Book inventory management',
+    version='',
+    packages=['book', 'book.migrations', 'bookstore'],
+    url='',
+    license='',
+    author='Prakash',
+    author_email='',
+    description=''
+)
